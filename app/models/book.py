f3d5581ba4e0ebdda38b4583f0e7b97a7f6b64dd
Helowiz <e.launay512@gmail.com>
2025-09-22 21:51:05 +0200

    La nouvelle BDD est en place

---
+++ app/models/book.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+from app.extensions import db
+
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150), nullable=False)
+    author = db.Column(db.String(100), nullable=False)
+    published_date = db.Column(db.DateTime, nullable=True, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"<Book {self.title}>"
