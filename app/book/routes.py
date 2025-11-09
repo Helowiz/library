4056c639460c1a03f71242b72b46b2a60acd76b5
Helowiz <e.launay512@gmail.com>
2025-11-09 13:29:14 +0100

    ajout du status

--- app/book/routes.py
+++ app/book/routes.py
@@ -1,9 +1,11 @@
-from flask import flash, redirect, render_template, url_for, abort
+from flask import flash, redirect, render_template, url_for, abort, request
 
 from app.book import bp
 from app.extensions import db
 from app.models.book import Book
 from app.forms import BookForm, DeleteBookForm 
+from app.models.reading import BookStatus, Reading
+
 
 
 # READ
@@ -19,7 +21,7 @@ def detail_book(book_id):
     book = Book.get_book_with_all_data(book_id)
     if not book:
         abort(404)
-    return render_template("book/detail.html", book=book, delete_form=delete_form)
+    return render_template("book/detail.html", book=book, delete_form=delete_form, BookStatus=BookStatus)
 
 # CREATE
 
@@ -28,19 +30,18 @@ def add_book():
     form = BookForm()
     
     if form.validate_on_submit():
-        new_book = Book(
+        new_book = Book.get_or_create(
             title=form.title.data,
             author=form.author.data,
             synopsis=form.synopsis.data,
-            url=form.url.data
+            url=form.url.data,
+            number_of_pages=form.number_of_pages.data
         )
-        
-        db.session.add(new_book)
-        db.session.commit()
-        
-        flash(f"Le livre '{new_book.title}' a été ajouté avec succès !", "success")
+
         return redirect(url_for('book.detail_book', book_id=new_book.id))
     return render_template('book/add.html', form=form)
+
+# UPDATE
 
 @bp.route('/edit/<int:book_id>/', methods=['GET', 'POST'])
 def edit_book(book_id):
@@ -55,6 +56,29 @@ def edit_book(book_id):
         return redirect(url_for('book.detail_book', book_id=book.id))
         
     return render_template('book/edit.html', form=form, book=book)
+
+@bp.route('/book/<int:book_id>/status', methods=['POST'])
+def update_status(book_id):
+    book = Book.query.get_or_404(book_id)
+    status_key = request.form.get('status')
+
+    if status_key in BookStatus.__members__:
+        
+        new_status = BookStatus[status_key]
+        
+        if not book.reading_status:
+            book.reading_status = Reading()
+
+        book.reading_status.status = new_status
+        db.session.commit()
+        
+        flash(f'Le statut du livre a été mis à jour : "{new_status.value}"', 'success')
+    else:
+        flash('Statut non valide.', 'danger')
+
+    return redirect(url_for('book.detail_book', book_id=book.id))
+
+# DELETE
 
 @bp.route('/delete/<int:book_id>', methods=['POST'])
 def delete_book(book_id):
